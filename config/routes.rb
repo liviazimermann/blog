e44389029244e6c7c1c232ba3d4423c65d25b00b
liviazimermann <zimermannlivia@gmail.com>
2024-11-07 11:49:55 -0300

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "articles#index"
 
+  resources :articles do
+    resources :comments
+  end
+
   get "/articles", to: "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
